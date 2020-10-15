f69d57227451122d6a0173e87a2fe93456bce313
Leeyoung888 <liyong1991com@163.com>
2020-10-15 10:47:25 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @author 名字
+ * @version 版本
+ * @company 公司
+ * @create 2020-10-15 10:43
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git!");
+    }
+}
